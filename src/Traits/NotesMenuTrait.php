8a9646be2711916a77b67a86cd5d6a91537b588b
ilBronza <davide_bronzini@hotmail.com>
2025-10-12 19:24:44 +0200

    all

--- src/Traits/NotesMenuTrait.php
+++ src/Traits/NotesMenuTrait.php
@@ -44,7 +44,7 @@ trait NotesMenuTrait
             return;
 
         $settingsButton = $menu->provideButton([
-                'text' => 'generals.settings',
+                'text' => 'menu::menu.settings',
                 'name' => 'settings',
                 'icon' => 'gear',
                 'roles' => ['administrator']
